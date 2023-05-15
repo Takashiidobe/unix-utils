5109370af9c94a88ecd701a00a67cefee66bacef
Takashi Idobe <mail@takashiidobe.com>
2023-05-15 17:01:32 -0400

    remove non compiling code from android

--- src/hostid.c
+++ src/hostid.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 int main(void) {
+#ifndef __ANDROID__
 	long host_id = gethostid();
 	printf("%.6ld\n", host_id);
+#endif
+  return 0;
 }
--- src/time.c
+++ src/time.c
@@ -3,9 +3,11 @@
 
 int main(int argc, char* argv[]) {
   int status;
+#ifndef __ANDROID__
   if (wait(&status) >= 0) {
     if (status) {
       printf("Child process exited with %d status\n", WEXITSTATUS(status));
     }
   }
+#endif
 }
